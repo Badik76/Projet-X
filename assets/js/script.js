33f59cd2fb4a80255a32a76ee00eb3e1991d2960
MehdiRouis <mehdi.rouis.1@laposte.net>
2018-10-19 17:00:37 +0200

    Préparation pour la suite

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,26 @@ $(function() {
 
   // Liste des catégories
   var categories = [
-    'eshop',
-    'forum',
-    'showcase',
-    'project',
-    'mobile'
-  ]
+    {name : 'E-Commerce', id : 'eshop'},
+    {name : 'Forum', id : 'forum'},
+    {name : 'Sites Vitrines', id : 'showcase'},
+    {name : 'Applications Mobile', id : 'mobile'},
+  ];
 
   // Liste des produits ( Si on en ajoute, l'HTML sera bien sûr aussi bien généré automatiquement <3)
   var products = [
     {ref: 1, product: 'GeniusBazaar', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782564956766209/Capture_decran_de_2018-10-19_11-55-18.png', description : 'GeniusBazaar est un site de vente en ligne d\'accessoires.', price: 500, montant : 1, category : 'eshop'},
     {ref: 2, product: 'MaBoutikBio', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782851620667393/Capture_decran_de_2018-10-19_11-50-11.png', description : 'MaBoutikBio est un site de vente en ligne de produits bio.', price:140, montant : 1, category : 'eshop'},
-    {ref: 3, product: 'MaDecoTendance', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502783602631770123/Capture_decran_de_2018-10-19_12-00-09.png', description : '', price: 99.99, montant : 1, category : 'eshop'},
-    {ref: 4, product: 'CMS', image: 'https://ressources.blogdumoderateur.com/2017/07/pretty-links-612x419.jpg', description : 'Ceci est une description intéressente!', price: 99.99, montant : 1, category : 'project'},
-    {ref: 5, product: 'Test', image: 'https://ressources.blogdumoderateur.com/2017/07/pretty-links-612x419.jpg', description : 'Ceci est une description intéressente!', price: 40, montant : 1, category : 'project'},
-    {ref: 5, product: 'Test2', image: 'https://ressources.blogdumoderateur.com/2017/07/pretty-links-612x419.jpg', description : 'Ceci est une description intéressente!', price: 15, montant : 1, category : 'project'},
-    {ref: 5, product: 'CMS de ouf', image: 'https://ressources.blogdumoderateur.com/2017/07/pretty-links-612x419.jpg', description : 'Ceci est une description intéressente!', price: 15, montant : 1, category : 'project'},
-    {ref: 5, product: 'CMS de ouf2', image: 'https://ressources.blogdumoderateur.com/2017/07/pretty-links-612x419.jpg', description : 'Ceci est une description intéressente!', price: 15, montant : 1, category : 'project'}
+    {ref: 3, product: 'MaDecoTendance', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502783602631770123/Capture_decran_de_2018-10-19_12-00-09.png', description : 'MaDecoTendance est un site de vente en ligne de décoration.', price: 99.99, montant : 1, category : 'eshop'},
+    {ref: 1, product: 'GeniusBazaar', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782564956766209/Capture_decran_de_2018-10-19_11-55-18.png', description : 'GeniusBazaar est un site de vente en ligne d\'accessoires.', price: 500, montant : 1, category : 'forum'},
+    {ref: 2, product: 'MaBoutikBio', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782851620667393/Capture_decran_de_2018-10-19_11-50-11.png', description : 'MaBoutikBio est un site de vente en ligne de produits bio.', price:140, montant : 1, category : 'forum'},
+    {ref: 3, product: 'MaDecoTendance', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502783602631770123/Capture_decran_de_2018-10-19_12-00-09.png', description : 'MaDecoTendance est un site de vente en ligne de décoration.', price: 99.99, montant : 1, category : 'forum'},
+    {ref: 1, product: 'GeniusBazaar', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782564956766209/Capture_decran_de_2018-10-19_11-55-18.png', description : 'GeniusBazaar est un site de vente en ligne d\'accessoires.', price: 500, montant : 1, category : 'showcase'},
+    {ref: 2, product: 'MaBoutikBio', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782851620667393/Capture_decran_de_2018-10-19_11-50-11.png', description : 'MaBoutikBio est un site de vente en ligne de produits bio.', price:140, montant : 1, category : 'showcase'},
+    {ref: 3, product: 'MaDecoTendance', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502783602631770123/Capture_decran_de_2018-10-19_12-00-09.png', description : 'MaDecoTendance est un site de vente en ligne de décoration.', price: 99.99, montant : 1, category : 'showcase'},
+    {ref: 1, product: 'GeniusBazaar', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782564956766209/Capture_decran_de_2018-10-19_11-55-18.png', description : 'GeniusBazaar est un site de vente en ligne d\'accessoires.', price: 500, montant : 1, category : 'mobile'},
+    {ref: 2, product: 'MaBoutikBio', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502782851620667393/Capture_decran_de_2018-10-19_11-50-11.png', description : 'MaBoutikBio est un site de vente en ligne de produits bio.', price:140, montant : 1, category : 'mobile'},
+    {ref: 3, product: 'MaDecoTendance', image: 'https://cdn.discordapp.com/attachments/502377233143562252/502783602631770123/Capture_decran_de_2018-10-19_12-00-09.png', description : 'MaDecoTendance est un site de vente en ligne de décoration.', price: 99.99, montant : 1, category : 'mobile'},
   ];
 
   // Fonction ajout du produit dans le panier
@@ -115,7 +118,6 @@ $(function() {
       deleteOnCart(id);
       deleteObjects(id + 1);
     });
-
   }
 
   // Mettre à jour le contenu du montant et du prix en fonction de l'id de la ligne
@@ -133,12 +135,21 @@ $(function() {
   function deleteOnCart(id) {
     delete cart[id];
   }
+
+  // Génération des catégories
+  for(var cats = 0; cats < categories.length; cats++) {
+    categ = categories[cats];
+    $('#categoryList').append(`
+      <a id="${categ.id}" class="waves-effect waves-dark btn white dark-blue-text">${categ.name}</a>
+    `)
+  }
+
   // Génération des boutons ajouter au panier
   for(var prods = 0; prods < products.length; prods++) {
     prod = products[prods];
     $('#productList').append(`
-      <div class="col s12 m6 l4 xl3">
-      <div class="card">
+      <div class="col s12 m4">
+      <div class="card" data-item-category="${prod.category}">
       <div class="card-image">
       <img class="responsive-img-products" src="${prod.image}">
       </div>
@@ -169,106 +180,106 @@ $(function() {
     });
     // End Mehdi's part2
 
-  // Karl's part
- $('.parallax').parallax(); //activation du parallax
+    // Karl's part
+    $('.parallax').parallax(); //activation du parallax
 
-// création d'un background animé
- var starcountsmall = 100;
-	var starglowsmallc = 0;
+    // création d'un background animé
+    var starcountsmall = 100;
+    var starglowsmallc = 0;
 
-	var starcountmedium = 50;
-	var starglowmediumc = 0;
+    var starcountmedium = 50;
+    var starglowmediumc = 0;
 
-	var starcountlarge = 20;
-	var starglowlargec = 0;
+    var starcountlarge = 20;
+    var starglowlargec = 0;
 
-	var rheight;
-	var rwidth;
-	for(var i = 0; i < starcountsmall; i++) {
-		starglowsmallc++;
-		rheight = Math.floor(Math.random() * 5998) + 1;
-		rwidth = Math.floor(Math.random() * 99) + 1;
-		if(starglowsmallc == 10)
-		{
-			$(".stars__control--small").append('<span class="star__small star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-			starglowsmallc = 0;
-		}
-		else
-		{
-			$(".stars__control--small").append('<span class="star__small" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-		}
-	}
+    var rheight;
+    var rwidth;
+    for(var i = 0; i < starcountsmall; i++) {
+      starglowsmallc++;
+      rheight = Math.floor(Math.random() * 5998) + 1;
+      rwidth = Math.floor(Math.random() * 90) + 1;
+      if(starglowsmallc == 10)
+      {
+        $(".stars__control--small").append('<span class="star__small star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+        starglowsmallc = 0;
+      }
+      else
+      {
+        $(".stars__control--small").append('<span class="star__small" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+      }
+    }
 
-	for(var i = 0; i < starcountmedium; i++) {
-		starglowmediumc++;
-		rheight = Math.floor(Math.random() * 5998) + 1;
-		rwidth = Math.floor(Math.random() * 99) + 1;
-		if(starglowmediumc == 7)
-		{
-			$(".stars__control--medium").append('<span class="star__medium star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-			starglowmediumc = 0;
-		}
-		else
-		{
-			$(".stars__control--medium").append('<span class="star__medium" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-		}
-	}
+    for(var i = 0; i < starcountmedium; i++) {
+      starglowmediumc++;
+      rheight = Math.floor(Math.random() * 5998) + 1;
+      rwidth = Math.floor(Math.random() * 90) + 1;
+      if(starglowmediumc == 7)
+      {
+        $(".stars__control--medium").append('<span class="star__medium star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+        starglowmediumc = 0;
+      }
+      else
+      {
+        $(".stars__control--medium").append('<span class="star__medium" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+      }
+    }
 
-	for(var i = 0; i < starcountlarge; i++) {
-		starglowlargec++;
-		rheight = Math.floor(Math.random() * 5998) + 1;
-		rwidth = Math.floor(Math.random() * 99) + 1;
-		if(starglowlargec == 3)
-		{
-			$(".stars__control--large").append('<span class="star__large star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-			starglowlargec = 0;
-		}
-		else
-		{
-			$(".stars__control--large").append('<span class="star__large" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
-		}
-	} // fin de la fonction pour le background
-   //End Karl's part
+    for(var i = 0; i < starcountlarge; i++) {
+      starglowlargec++;
+      rheight = Math.floor(Math.random() * 5998) + 1;
+      rwidth = Math.floor(Math.random() * 90) + 1;
+      if(starglowlargec == 3)
+      {
+        $(".stars__control--large").append('<span class="star__large star__glow" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+        starglowlargec = 0;
+      }
+      else
+      {
+        $(".stars__control--large").append('<span class="star__large" style="top:'+ rheight +'px;left:' + rwidth +'vw;"></span>');
+      }
+    } // fin de la fonction pour le background
+    //End Karl's part
 
-  // Manouel's part
+    // Manouel's part
 
-  $('.carousel').carousel({
-    indicators: true,
-  });
-  setInterval (function() {
-        $('.carousel').carousel('next');
-  }, 4000);
+    $('.carousel').carousel({
+      indicators: true,
+    });
+    setInterval (function() {
+      $('.carousel').carousel('next');
+    }, 4000);
 
-  // End of Manouel's
+    // End of Manouel's
 
 
     // Magalie's part
     $("#img1").mouseover(function() {
-        img1.src = "assets/img/avatar2.jpg";
-      })
-      $("#img1").mouseout(function() {
-        img1.src = "assets/img/avatar.jpg";
-      })
+      img1.src = "assets/img/avatar2.jpg";
+    })
+    $("#img1").mouseout(function() {
+      img1.src = "assets/img/avatar.jpg";
+    })
 
-      $("#img2").mouseover(function() {
-        img2.src = "assets/img/avatar2.jpg";
-      })
-      $("#img2").mouseout(function() {
-        img2.src = "assets/img/avatar.jpg";
-      })
+    $("#img2").mouseover(function() {
+      img2.src = "assets/img/avatar2.jpg";
+    })
+    $("#img2").mouseout(function() {
+      img2.src = "assets/img/avatar.jpg";
+    })
 
-      $("#img3").mouseover(function() {
-        img3.src = "assets/img/avatar2.jpg";
-      })
-      $("#img3").mouseout(function() {
-        img3.src = "assets/img/avatar.jpg";
-      })
+    $("#img3").mouseover(function() {
+      img3.src = "assets/img/avatar2.jpg";
+    })
+    $("#img3").mouseout(function() {
+      img3.src = "assets/img/avatar.jpg";
+    })
 
-      $("#img4").mouseover(function() {
-        img4.src = "assets/img/avatar2.jpg";
-      })
-      $("#img4").mouseout(function() {
-        img4.src = "assets/img/avatar.jpg";
-      })
+    $("#img4").mouseover(function() {
+      img4.src = "assets/img/avatar2.jpg";
+    })
+    $("#img4").mouseout(function() {
+      img4.src = "assets/img/avatar.jpg";
+    })
     // End Magalie's part
   });
