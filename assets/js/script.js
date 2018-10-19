a8f9ef19f1850e454132dca8add675782b7c6311
Magalie-K <magalie.kabelaan@gmail.com>
2018-10-19 11:20:22 +0200

    pas de changements

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,42 +29,32 @@ $(function() {
   // End Mehdi's part
 
   // Magalie's part
-  // $("#img1").mouseover(function() {
-  //   img1.src = "assets/img/avatar2.jpg";
-  // })
-  // $("#img1").mouseout(function() {
-  //   img1.src = "assets/img/avatar.jpg";
-  // })
-  //
-  // $("#img2").mouseover(function() {
-  //   img2.src = "assets/img/avatar2.jpg";
-  // })
-  // $("#img2").mouseout(function() {
-  //   img2.src = "assets/img/avatar.jpg";
-  // })
-  //
-  // $("#img3").mouseover(function() {
-  //   img3.src = "assets/img/avatar2.jpg";
-  // })
-  // $("#img3").mouseout(function() {
-  //   img3.src = "assets/img/avatar.jpg";
-  // })
-  //
-  // $("#img4").mouseover(function() {
-  //   img4.src = "assets/img/avatar2.jpg";
-  // })
-  // $("#img4").mouseout(function() {
-  //   img4.src = "assets/img/avatar.jpg";
-  // })
-
-
-
-
   $("#img1").mouseover(function() {
     img1.src = "assets/img/avatar2.jpg";
   })
   $("#img1").mouseout(function() {
     img1.src = "assets/img/avatar.jpg";
+  })
+
+  $("#img2").mouseover(function() {
+    img2.src = "assets/img/avatar2.jpg";
+  })
+  $("#img2").mouseout(function() {
+    img2.src = "assets/img/avatar.jpg";
+  })
+
+  $("#img3").mouseover(function() {
+    img3.src = "assets/img/avatar2.jpg";
+  })
+  $("#img3").mouseout(function() {
+    img3.src = "assets/img/avatar.jpg";
+  })
+
+  $("#img4").mouseover(function() {
+    img4.src = "assets/img/avatar2.jpg";
+  })
+  $("#img4").mouseout(function() {
+    img4.src = "assets/img/avatar.jpg";
   })
 
 
